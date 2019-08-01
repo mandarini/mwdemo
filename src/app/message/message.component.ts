9881655aad2f3ff6a2c90ae141f3b90702d4a0fb
Katerina Skroumpelou <sk.katherine@gmail.com>
2019-08-01 19:23:06 +0300

    final changes

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Message } from '../message';
+import { Component, OnInit, Input } from "@angular/core";
+import { Message } from "../message";
 
 @Component({
-  selector: 'app-message',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+  selector: "app-message",
+  templateUrl: "./message.component.html",
+  styleUrls: ["./message.component.scss"]
 })
 export class MessageComponent implements OnInit {
-
   @Input() message: Message;
   @Input() userAuth: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.message, this.userAuth);
   }
-
 }
